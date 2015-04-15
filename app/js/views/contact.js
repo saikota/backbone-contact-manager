af1065da3a63fd37d70c526bc81b33712cda5b6a
Sai Kota Mac <sai.kota@hotmail.com>
2015-04-15 00:40:16 -0700

    updated the view to show  phone number and email

--- app/js/views/contact.js
+++ app/js/views/contact.js
@@ -2,8 +2,11 @@
  * Created by Sai on 4/14/15.
  */
 ContactManager.Views.Contact=Backbone.View.extend({
+    tagName:'li',
+    className:'media col-md-6 col-lg-4',
+    template: _.template($('#tpl-contact').html()),
     render:function(){
-        var html='<h1>'+ this.model.get('name') +'</h1>';
+        var html=this.template(this.model.toJSON());
         this.$el.html(html);
         return this;
     }
