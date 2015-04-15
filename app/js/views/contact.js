a53409ef838c50deabf50eb540f18bd8f7262c2b
Sai Kota Mac <sai.kota@hotmail.com>
2015-04-15 00:08:32 -0700

    initial version of View

---
+++ app/js/views/contact.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Sai on 4/14/15.
+ */
+ContactManager.Views.Contact=Backbone.View.extend({
+    render:function(){
+        var html='<h1>'+ this.model.get('name') +'</h1>';
+        this.$el.html(html);
+        return this;
+    }
+});
