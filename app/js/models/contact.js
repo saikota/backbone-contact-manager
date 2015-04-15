0547cc923ba84fc68eccd3e2007253cd4bf077de
Sai Kota Mac <sai.kota@hotmail.com>
2015-04-14 23:40:32 -0700

    Added Skeleton for contact model including the defaults.

--- app/js/models/contact.js
+++ app/js/models/contact.js
@@ -1,3 +1,14 @@
 /**
  * Created by Sai on 4/14/15.
  */
+ContactManager.Models.contact=Backbone.Model.extend({
+    defaults:{
+        name:null,
+        tel:null,
+        email:null,
+        avatar:null
+    },
+    initialize:function(){
+        this.set('avatar', _.random(1,15)+'.jpg');
+    }
+});
