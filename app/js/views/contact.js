863a935c95871d137f8f446916587ceecc9d09a2
Sai Kota Mac <sai.kota@hotmail.com>
2015-04-15 23:34:46 -0700

    Added delete functionality to the Contact Manger.

--- app/js/views/contact.js
+++ app/js/views/contact.js
@@ -3,6 +3,16 @@
  */
 ContactManager.Views.Contact=Backbone.View.extend({
     tagName:'li',
+    events:{
+        'click .delete-contact':'onClickDelete'
+    },
+    initialize:function(){
+        this.listenTo(this.model,'remove',this.remove);
+    },
+    onClickDelete:function(e){
+        e.preventDefault();
+        this.model.collection.remove(this.model);
+    },
     className:'media col-md-6 col-lg-4',
     template: _.template($('#tpl-contact').html()),
     render:function(){
