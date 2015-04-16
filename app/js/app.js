663526f5e5a46bcd7e1333e9784daa5d83bc0914
Sai Kota Mac <sai.kota@hotmail.com>
2015-04-15 23:09:19 -0700

    Added routes to route the application to a particular view.

--- app/js/app.js
+++ app/js/app.js
@@ -5,9 +5,28 @@ window.ContactManager={
 
     start: function(data) {
         var contacts=new ContactManager.Collections.Contacts(data.contacts);
+        var router=new ContactManager.Router();
 
-        var contactsView=new ContactManager.Views.Contacts({collection:contacts});
 
-        $('.main-container').html(contactsView.render().$el);
+        router.on('route:home', function () {
+            router.navigate('contacts', {trigger: true, replace: true});
+
+        });
+
+        router.on('route:showContacts', function () {
+            var contactsView = new ContactManager.Views.Contacts({collection: contacts});
+            $('.main-container').html(contactsView.render().$el);
+        });
+
+        router.on('route:newContact',function(){
+           console.log(" routing to new contact");
+        });
+
+        router.on('route:editContact',function(){
+            console.log(" routing to edit a contact");
+        });
+
+        Backbone.history.start();
+
     }
 };
